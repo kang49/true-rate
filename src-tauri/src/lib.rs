bf9ef32a008989b91f50a5c61e3300d0baf95e34
Kankawee Aramrak <a@kankawee.uk>
2025-09-26 20:27:28 +1000

    Add target currencies and request wise API with Rust

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,100 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    log::info!("Tauri is awesome!");
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use reqwest::Error;
+use serde::Deserialize;
+
+// Structs
+#[derive(Deserialize)]
+struct Currency {
+    code: String,
+    amount: Option<f32>, // amount might be null
 }
+
+#[derive(Deserialize, Debug)]
+struct Comparison {
+    #[serde(rename = "sourceCurrency")]
+    source_currency: String,
+
+    #[serde(rename = "targetCurrency")]
+    target_currency: String,
+
+    amount: f64,
+
+    #[serde(rename = "amountType")]
+    amount_type: String,
+
+    providers: Vec<Provider>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Provider {
+    id: u32,
+    alias: String,
+    name: String,
+    quotes: Vec<Quote>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Quote {
+    rate: f64,
+    fee: f64,
+    #[serde(rename = "receivedAmount")]
+    received_amount: f64,
+    #[serde(rename = "sendAmount")]
+    send_amount: Option<f64>,
+}
+
+
+
+#[tauri::command]
+fn greet(origincurrency: Currency, targetcurrencies: Vec<Currency>) -> f32 {
+    log::info!("Tauri is awesome!");
+    log::info!("Origin: {} {:?}", origincurrency.code, origincurrency.amount);
+    log::info!("Target: {} {:?}", targetcurrencies[0].code, targetcurrencies[0].amount);
+
+    let send_amount = origincurrency.amount.unwrap_or(0.0) as f64;
+    let target_currency = &targetcurrencies[0].code;
+
+    let rt = tokio::runtime::Runtime::new().unwrap();
+
+    let result: f32 = rt.block_on(async {
+        let url = format!(
+            "https://api.wise.com/v4/comparisons/?sourceCurrency={}&targetCurrency={}&sendAmount={}",
+            origincurrency.code, target_currency, send_amount
+        );
+
+        match reqwest::get(&url).await {
+            Ok(resp) => {
+                let json: Result<Comparison, _> = resp.json().await;
+                match json {
+                    Ok(data) => {
+                        log::info!("Received comparison: {:?}", data);
+        
+                        // request wise rate
+                        if let Some(provider) = data.providers.iter().find(|p| p.alias == "wise") {
+                            if let Some(quote) = provider.quotes.first() {
+                                return quote.rate as f32;
+                            }
+                        }
+        
+                        0.0
+                    }
+                    Err(e) => {
+                        log::error!("Failed to parse JSON: {}", e);
+                        0.0
+                    }
+                }
+            }
+            Err(e) => {
+                log::error!("Request failed: {}", e);
+                0.0
+            }
+        }
+    });
+
+    result
+}
+
+
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@
         <h4 class="w-full flex justify-center text-white text-[22px] font-bold  pt-[50px]">
           Origin
         </h4>
+        <!-- Origin Input -->
         <div class="w-full flex justify-center pt-[20px]">
           <div class="relative w-[100px]">
             <!-- Dropdown trigger -->
@@ -45,7 +46,49 @@
             </div>
           </div>
           <!-- Origin Input -->
-          <input v-model="origin_currency.amount" @change="Calculator('origin', origin_currency.amount)" class="w-[300px] h-[40px] bg-white/10 rounded-r-[10px] outline-none text-white font-bold" type="text"></input>
+          <input v-model="origin_currency.amount" @change="Calculator('origin', origin_currency.amount)" class="w-[300px] h-[40px] bg-white/10 rounded-r-[10px] outline-none text-white font-bold" placeholder="amount" type="text"></input>
+        </div>
+
+        <h4 class="w-full flex justify-center text-white text-[22px] font-bold  pt-[50px]">
+          Targets
+        </h4>
+        <!-- Target Inputs -->
+         <div class="w-full grid gap-y-[20px] pt-[20px]" :class="{'grid-cols-3': target_currencies.length >= 3, 'grid-cols-2': target_currencies.length === 2, 'grid-cols-1 place-items-center': target_currencies.length === 1}">
+          <div v-for="i in target_currencies" class="w-full flex justify-center pt-[20px]" :class="{'ml-[12px]': target_currencies.length >= 3 && C_dropdown[C_dropdown.length - 1].where === i.code}">
+            <div class="relative w-[100px]">
+              <!-- Dropdown trigger -->
+              <div class="w-[100px] h-[40px] bg-white/10 rounded-l-[10px] flex items-center px-[10px] cursor-pointer"
+                @click="toggleDropdown(i.code)">
+                <div class="flex items-center">
+                  <i class="rounded-[3px]" :class="currencies.find(currencies => currencies.code === i.code)?.icon"></i>
+                  <h4 class="text-white font-bold ml-[5px] select-none">{{ i.code }}</h4>
+                </div>
+                <div class="w-full text-right">
+                  <i class="fas fa-angle-down text-white transition-transform duration-200"
+                    :class="{ 'rotate-180': C_dropdown.find(dropdown => dropdown.where === i.code)?.isOpen }"></i>
+                </div>
+              </div>
+
+              <!-- Dropdown menu -->
+              <div v-if="C_dropdown.find(dropdown => dropdown.where === i.code)?.isOpen"
+                class="max-h-[300px] absolute top-[45px] left-0 w-full bg-white/20 rounded-[10px] select-none overflow-scroll scrollbar-hide shadow-md backdrop-blur-sm z-10">
+                <ul>
+                  <li v-for="(currency, index) in currencies" :key="index" @click="selectCurrency(currency, i.code), toggleDropdown(i.code)"
+                    class="flex items-center px-[10px] py-[8px] cursor-pointer hover:bg-white/30 rounded-[8px]">
+                    <i :class="currency.icon + ' rounded-[3px]'"></i>
+                    <span class="text-white font-medium ml-[5px]">{{ currency.code }}</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <!-- Origin Input -->
+            <input v-model="i.amount" @change="Calculator(i.code, i.amount)" class="w-[200px] h-[40px] bg-white/10 rounded-r-[10px] outline-none text-white font-bold" placeholder="amount" type="text"></input>
+            
+            <!-- Add Target Button -->
+            <button v-show="target_currencies[target_currencies.length - 1].code === i.code" @click="addTargetCurrency()" class="flex items-center text-white/50 text-[20px]">
+              <i class="far fa-plus-circle ml-[5px]"></i>
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -64,10 +107,11 @@ import {
   attachConsole,
   attachLogger,
 } from '@tauri-apps/plugin-log';
+import { Currency } from './interface/types.ts';
 
 let C_dropdown = ref([{ where: "origin", isOpen: false }, { where: "AUD", isOpen: false }]);
-let origin_currency = ref({ code: "USD", amount: 0 });
-let target_currencies = ref([{ code: "EUR", amount: 0 }, { code: "GBP", amount: 0 }]);
+let origin_currency = ref<Currency>({ code: "USD", amount: null as number | null });
+let target_currencies = ref<Currency[]>([{ code: "AUD", amount: null as number | null }]);
 
 const currencies = [
   { code: "AUD", icon: "fi fi-au" },
@@ -103,10 +147,11 @@ const currencies = [
   { code: "CNY", icon: "fi fi-cn" },
 ];
 
-function Calculator(where: string, raw_amount: number) {
-  debug(raw_amount.toString());
-  debug(where);
-
+async function Calculator(where: string, raw_amount: number | null) {
+  if (raw_amount === null) {
+    warn(`Raw amount is null for ${where}.`);
+    return;
+  }
   const result: number = new Function("return " + raw_amount.toString())();
   if (where === 'origin') {
     origin_currency.value.amount = result;
@@ -118,7 +163,14 @@ function Calculator(where: string, raw_amount: number) {
       warn(`Target currency with code="${where}" not found.`);
     }
   }
-  debug(result.toString());
+  debug(`Calculated amount for ${where}: ${raw_amount} = ${result}${where}`);
+
+  // Call the Rust command
+  const response = await invoke<number>("greet", {
+    origincurrency: origin_currency.value,
+    targetcurrencies: target_currencies.value
+  });
+  debug(`Response from Rust: ${response}`);
 }
 
 function toggleDropdown(where: string) {
@@ -126,26 +178,37 @@ function toggleDropdown(where: string) {
   if (dropdown) {
     dropdown.isOpen = !dropdown.isOpen;
   } else {
-    warn(`Dropdown with where="${where}" not found.`);
+    console.warn(`Dropdown with where="${where}" not found.`);
   }
 }
 
-async function selectCurrency(currency: { code: string; icon: string }, where: "origin" | "target") {
-  switch (where) {
-    case "origin":
-      origin_currency.value.code = currency.code;
-      // Call the Rust command
-      const response: string = await invoke<string>("greet", {
-        name: origin_currency.value.code,
-      });
-      debug(`Response from Rust: ${response}`);
-      debug(C_dropdown.value.find(i => i.where === "origin")?.isOpen.toString() || "false");
-      break;
-    case "target":
-      if (!target_currencies.value.includes({ code: currency.code, amount: 0 })) {
-        target_currencies.value.push({ code: currency.code, amount: 0 });
-      }
-      break;
+function selectCurrency(currency: { code: string; icon: string }, where: string) {
+  if (where === 'origin') {
+    origin_currency.value.code = currency.code;
+  } else if (where === target_currencies.value.find(i => i.code === where)?.code) {
+    const target = target_currencies.value.find(i => i.code === where);
+    if (target) {
+      target.code = currency.code;
+      C_dropdown.value.find(i => i.where === where)!.where = currency.code;
+    } else {
+      warn(`Target currency with code="${where}" not found.`);
+    }
+  } else {
+    warn(`Where="${where}" does not match any known currency slots.`);
+  }
+}
+
+function addTargetCurrency() {
+  //random currency not already in target_currencies and push it to target_currencies
+  let filltered_currencies = currencies.filter(currency => !target_currencies.value.some(target => target.code === currency.code) && currency.code !== origin_currency.value.code);
+  if (filltered_currencies.length === 0) {
+    warn("No more currencies to add.");
+    return;
+  } else {
+    let random_currency = filltered_currencies[Math.floor(Math.random() * filltered_currencies.length)];
+    target_currencies.value.push({ code: random_currency.code, amount: null });
+    C_dropdown.value.push({ where: random_currency.code, isOpen: false });
+    debug(`Added target currency: ${random_currency.code}`);
   }
 }
 </script>
